1e7951f89cad11562535cab08b476d9ac44d510d
鱼梦江湖 <zyyypx@qq.com>
2025-05-04 09:40:40 +0800

    deploy

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// 顶级属性自动解包
+const obj = {
+  str:ref("Hello")
+}
+</script>
+
+<template>
+<h1>{{ obj.str }}</h1>
+</template>
+
+<style scoped></style>
+  
